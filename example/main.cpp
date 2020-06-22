b1d477ecf9bb9b6aebd2969dfa8cd4e89d40a8d0
Martin Skalsky <martin.skalsky@artin.cz>
2020-06-22 12:30:08 +0200

    added signal handlers to example plus minor improvement in closing

--- example/main.cpp
+++ example/main.cpp
@@ -6,37 +6,61 @@
 
 #include <RoboCore/RTK/NtripClient.h>
 
+#include <execinfo.h>
+#include <unistd.h>
 
-void callback(const  std::shared_ptr<const RoboCore::Sensor::GPS::Data>& data){
-   // std::cout << data->position.latitude << std::endl;
-   // std::cout << data->position.longitude<< std::endl;
-   // std::cout << static_cast<int>(data->fixType) << std::endl;
+#include <experimental/filesystem>
+
+void callback(const std::shared_ptr<const RoboCore::Sensor::GPS::Data>& data) {
+    std::cout << data->position.latitude << std::endl;
+    std::cout << data->position.longitude << std::endl;
+    std::cout << static_cast<int>(data->fixType) << std::endl;
 }
 
-int main(){
-    while( 1) {
-        std::cout << "start" << std::endl;
-        {
-            sleep(1);
-            RoboCore::Core core;
-            auto context = std::make_shared<RoboCore::Context::RealTime>();
+std::shared_ptr<RoboCore::Core> core;
 
-            context->getLogger().setDefaultVerbosity(RoboCore::Logging::Verbosity::All);
-            core.setContext(context);
-            auto contextInjector = RoboCore::withContext(context);
-            (void)contextInjector; // TODO: this is necessary to correctly pass context however as it's not used, clang-tidy reports an error
+void segvHandler(int sig) {
+    void* array[10];
+    auto size = backtrace(array, 10);
 
-            auto gps = core.addWorker<Ublox::UbloxGPS>("/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00", 115200, ublox_msgs::CfgPRT::PROTO_UBX, ublox_msgs::CfgPRT::PROTO_UBX);
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
 
-            auto ntripClient = core.addWorker<RoboCore::RTK::NtripClient>("195.245.209.181", 2111, "roboauto", "robo.Auto1", "/RTK3-MSM");
-            RoboCore::connect(gps->GGANMEAMsgProvider, ntripClient, &RoboCore::RTK::NtripClient::onPosition);
-            RoboCore::connect(ntripClient->rtkProvider, gps, &Ublox::UbloxGPS::onRTKUpdate);
+    core.reset();
+    exit(sig);
+}
 
-            gps->dataProvider.addCallback(std::function<void(const std::shared_ptr<const RoboCore::Sensor::GPS::Data>&)>(callback));
+void termHandler(int sig) {
+    std::cout << "LAMA received SIGTERM " << sig << std::endl;
+    core.reset();
 
-            core(RoboCore::Core::ExecutionType::NonBlocking);
-            sleep(1);
-        }
+    exit(sig);
+}
+
+int main(int argc, char* argv[]) {
+    if (signal(SIGINT, termHandler) == SIG_ERR) {
+        std::cout << "SIGINT" << std::endl;
+    };
+
+    {
+        core = std::make_shared<RoboCore::Core>();
+        auto context = std::make_shared<RoboCore::Context::RealTime>();
+
+        context->getLogger().setDefaultVerbosity(RoboCore::Logging::Verbosity::All);
+        core->setContext(context);
+        auto contextInjector = RoboCore::withContext(context);
+        (void)contextInjector; // TODO: this is necessary to correctly pass context however as it's not used, clang-tidy reports an error
+
+        auto gps = core->addWorker<Ublox::UbloxGPS>("/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00", 115200, ublox_msgs::CfgPRT::PROTO_UBX, ublox_msgs::CfgPRT::PROTO_UBX);
+        auto ntripClient = core->addWorker<RoboCore::RTK::NtripClient>("195.245.209.181", 2111, "roboauto", "robo.Auto1", "/RTK3-MSM");
+        RoboCore::connect(gps->GGANMEAMsgProvider, ntripClient, &RoboCore::RTK::NtripClient::onPosition);
+        RoboCore::connect(ntripClient->rtkProvider, gps, &Ublox::UbloxGPS::onRTKUpdate);
+
+        gps->dataProvider.addCallback(std::function<void(const std::shared_ptr<const RoboCore::Sensor::GPS::Data>&)>(callback));
     }
+
+    (*core)(RoboCore::Core::ExecutionType::NonBlocking);
+    while (1) {
+    };
     return 0;
 }
